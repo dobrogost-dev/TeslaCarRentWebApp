2452d04823f76a9fc443841ca375cbcc52ccf4a8
Dobrogost <dobrogost.adrian@gmail.com>
2023-10-18 05:22:23 +0200

    Prepared a base for the project

---
+++ TeslaCarRentWebApp/ClientApp/src/app/browse-cars/browse-cars.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-browse-cars',
+  templateUrl: './browse-cars.component.html',
+  styleUrls: ['./browse-cars.component.css']
+})
+export class BrowseCarsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
