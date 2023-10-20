8935af7770b992f540a80cf906c7d7c9fc894616
Dobrogost <dobrogost.adrian@gmail.com>
2023-10-20 08:03:58 +0200

    Added initial models for the cars and orders

---
+++ TeslaCarRentWebApp/Models/Car.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TeslaCarRentWebApp.Models
+{
+    [Table("Car")] 
+    public class Car
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Model { get; set; } = null!;
+        [Required]
+        public int NumberOfSeats { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+    }
+}
---
+++ TeslaCarRentWebApp/Models/Order.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TeslaCarRentWebApp.Models
+{
+    [Table("Order")]
+    public class Order
+    {
+        [Key]
+        public int Id { get; set; }
+        public int CarId { get; set; }
+        [Required]
+        public virtual Car OrderedCar { get; set; } = null!;
+        [Required]
+        public DateTime PickUpDate { get; set; }
+        [Required]
+        public DateTime DropOffDate { get; set; }
+        [Precision(10, 2), Required]
+        public decimal FinalPrice { get; set; }
+    }
+}
